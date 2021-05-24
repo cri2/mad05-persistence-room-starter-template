3c24e75d96ae61a1e7ab6a2b092049bb375c208f
cri2 <77294962+cri2@users.noreply.github.com>
2021-05-24 20:55:18 +0200

    "3rd task AppDB"

--- app/src/main/java/com/example/mad03_fragments_and_navigation/database/AppDatabase.kt
+++ app/src/main/java/com/example/mad03_fragments_and_navigation/database/AppDatabase.kt
@@ -1,7 +1,36 @@
 package com.example.mad03_fragments_and_navigation.database
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.mad03_fragments_and_navigation.models.Movie
 
+//Source: https://developer.android.com/codelabs/kotlin-android-training-room-database#5
+
+@Database(entities = [Movie::class], version = 1, exportSchema = false)
 abstract class AppDatabase: RoomDatabase() {
-    // TODO implement me
+
+    abstract fun movieDao(): MovieDao
+
+    companion object {
+        @Volatile //never cached
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase{
+            val tempInstance = INSTANCE
+            if(tempInstance != null){
+                return tempInstance
+            }
+            synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "movie_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 }
