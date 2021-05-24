8ba970e1bbfc1f0a268b73b3e7bad89df64855a1
cri2 <77294962+cri2@users.noreply.github.com>
2021-05-24 20:36:31 +0200

    "2rd task implement DAO "

--- app/src/main/java/com/example/mad03_fragments_and_navigation/database/MovieDao.kt
+++ app/src/main/java/com/example/mad03_fragments_and_navigation/database/MovieDao.kt
@@ -1,5 +1,26 @@
 package com.example.mad03_fragments_and_navigation.database
 
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.mad03_fragments_and_navigation.models.Movie
+
+@Dao
 interface MovieDao {
-    // TODO implement me
+
+    // methods according template
+
+        @Insert
+        suspend fun createMovie(part: Movie)
+
+        @Update
+        suspend fun updateMovie(part: Movie)
+
+        @Delete
+        suspend fun deleteMovie(part: Movie)
+
+        @Query("DELETE FROM movie_data")
+        suspend fun clearTable()
+
+        @Query("SELECT * FROM movie_data ORDER BY id ASC")
+        fun getAll(): LiveData<List<Movie>>
 }
